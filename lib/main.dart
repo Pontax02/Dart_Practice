fff4431f7fc698470bbeb2a6d20e5d83e8a92c1a
Pontax02 <ppontanilla07@gmail.com>
2026-03-20 20:02:23 +0100

    chore(main): :alembic: Link OnboardingScreen to main

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_test/screens/onboarding_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,11 +12,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Twitch Clone',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const Text('Twitch Clone Home Page'),
+      home: const OnboardingScreen(),
     );
   }
 }
