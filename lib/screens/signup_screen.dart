8931b63d890817c9538bf0823a96c2c83bb84f5c
Pontax02 <ppontanilla07@gmail.com>
2026-03-26 19:29:56 +0100

    feat(signup_screen): Add textBoxes to the signup screen

--- lib/screens/signup_screen.dart
+++ lib/screens/signup_screen.dart
@@ -1,4 +1,5 @@
 import "package:flutter/material.dart";
+import "package:flutter_application_test/widgets/custom_textfield.dart";
 
 class SignupScreen extends StatefulWidget {
   const SignupScreen({super.key});
@@ -8,8 +9,53 @@ class SignupScreen extends StatefulWidget {
 }
 
 class _SignupScreenState extends State<SignupScreen> {
+  final TextEditingController _emailcontroller = TextEditingController();
+  final TextEditingController _passwordcontroller = TextEditingController();
+  final TextEditingController _usernamecontroller = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: const Text("Sign Up")));
+    final size = MediaQuery.of(context).size;
+    return Scaffold(
+      appBar: AppBar(title: const Text("Sign Up")),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(height: size.height * 0.1),
+              Text("Sign Up Screen"),
+              const Text(
+                "Email",
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 8.0),
+                child: CustomTextField(controller: _emailcontroller),
+              ),
+              const SizedBox(height: 20),
+              const Text(
+                "Username",
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 8.0),
+                child: CustomTextField(controller: _usernamecontroller),
+              ),
+              const SizedBox(height: 20),
+              const Text(
+                "Password",
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 8.0),
+                child: CustomTextField(controller: _passwordcontroller),
+              ),
+              const SizedBox(height: 20),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
