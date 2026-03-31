f5b95e02e32fbee50e3b9b771919b5c367686167
Pontax02 <ppontanilla07@gmail.com>
2026-03-31 18:02:34 +0200

    feat(auth_method): Add authentication method for signup

--- lib/resources/auth_methods.dart
+++ lib/resources/auth_methods.dart
@@ -1,9 +1,35 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import "package:flutter_application_test/models/user.dart" as model;
 
 class AuthMethods {
   final _userRef = FirebaseFirestore.instance.collection("users");
   final _auth = FirebaseAuth.instance;
 
-  signupUser() async {}
+  Future<bool> signUpUser(
+    String email,
+    String username,
+    String password,
+  ) async {
+    bool res = false;
+    try {
+      UserCredential cred = await _auth.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      if (cred.user != null) {
+        model.User user = model.User(
+          username: username.trim(),
+          email: email.trim(),
+          uid: cred.user!.uid,
+        );
+        await _userRef.doc(cred.user!.uid).set(user.toMap());
+
+        res = true;
+      }
+    } on FirebaseAuthException catch (e) {
+      print(e.message!);
+    }
+    return res;
+  }
 }
